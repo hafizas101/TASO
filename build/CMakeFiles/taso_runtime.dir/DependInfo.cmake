441077899a16f9a86b5369b4ddeb4eceeb2e7309
hafizas101 <hafizas101@gmail.com>
2021-07-21 16:04:33 +0000

    first

---
+++ build/CMakeFiles/taso_runtime.dir/DependInfo.cmake
@@ -0,0 +1,109 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CUDA"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CUDA
+  "/usr/TASO/src/cudnn/activation_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/activation_kernel.cu.o"
+  "/usr/TASO/src/cudnn/batchnorm_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/batchnorm_kernel.cu.o"
+  "/usr/TASO/src/cudnn/cast_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/cast_kernel.cu.o"
+  "/usr/TASO/src/cudnn/concat_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/concat_kernel.cu.o"
+  "/usr/TASO/src/cudnn/constant_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/constant_kernel.cu.o"
+  "/usr/TASO/src/cudnn/conv2d_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/conv2d_kernel.cu.o"
+  "/usr/TASO/src/cudnn/cuda_helper.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/cuda_helper.cu.o"
+  "/usr/TASO/src/cudnn/element_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/element_kernel.cu.o"
+  "/usr/TASO/src/cudnn/elementwise_unary_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/elementwise_unary_kernel.cu.o"
+  "/usr/TASO/src/cudnn/enlarge_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/enlarge_kernel.cu.o"
+  "/usr/TASO/src/cudnn/matmul_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/matmul_kernel.cu.o"
+  "/usr/TASO/src/cudnn/merge_gconv_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/merge_gconv_kernel.cu.o"
+  "/usr/TASO/src/cudnn/mul_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/mul_kernel.cu.o"
+  "/usr/TASO/src/cudnn/ops_cudnn.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/ops_cudnn.cu.o"
+  "/usr/TASO/src/cudnn/pad_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/pad_kernel.cu.o"
+  "/usr/TASO/src/cudnn/pool2d_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/pool2d_kernel.cu.o"
+  "/usr/TASO/src/cudnn/reduce_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/reduce_kernel.cu.o"
+  "/usr/TASO/src/cudnn/reshape_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/reshape_kernel.cu.o"
+  "/usr/TASO/src/cudnn/resize_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/resize_kernel.cu.o"
+  "/usr/TASO/src/cudnn/shape_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/shape_kernel.cu.o"
+  "/usr/TASO/src/cudnn/slice_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/slice_kernel.cu.o"
+  "/usr/TASO/src/cudnn/squeeze_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/squeeze_kernel.cu.o"
+  "/usr/TASO/src/cudnn/topk_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/topk_kernel.cu.o"
+  "/usr/TASO/src/cudnn/transpose_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/transpose_kernel.cu.o"
+  "/usr/TASO/src/cudnn/unsqueeze_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/unsqueeze_kernel.cu.o"
+  "/usr/TASO/src/cudnn/where_kernel.cu" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/cudnn/where_kernel.cu.o"
+  )
+set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CUDA
+  "USE_CUDNN"
+  "taso_runtime_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CUDA_TARGET_INCLUDE_PATH
+  "."
+  "../include"
+  "/opt/conda/include"
+  "/usr/local/cuda/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/TASO/build/rules.pb.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/rules.pb.cc.o"
+  "/usr/TASO/src/core/activation.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/activation.cc.o"
+  "/usr/TASO/src/core/batchnorm.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/batchnorm.cc.o"
+  "/usr/TASO/src/core/cast.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/cast.cc.o"
+  "/usr/TASO/src/core/concat.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/concat.cc.o"
+  "/usr/TASO/src/core/constant.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/constant.cc.o"
+  "/usr/TASO/src/core/conv2d.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/conv2d.cc.o"
+  "/usr/TASO/src/core/element.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/element.cc.o"
+  "/usr/TASO/src/core/elementwise_unary.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/elementwise_unary.cc.o"
+  "/usr/TASO/src/core/enlarge.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/enlarge.cc.o"
+  "/usr/TASO/src/core/graph_to_trt.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/graph_to_trt.cc.o"
+  "/usr/TASO/src/core/matmul.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/matmul.cc.o"
+  "/usr/TASO/src/core/merge_gconv.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/merge_gconv.cc.o"
+  "/usr/TASO/src/core/mul.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/mul.cc.o"
+  "/usr/TASO/src/core/noop.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/noop.cc.o"
+  "/usr/TASO/src/core/ops.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/ops.cc.o"
+  "/usr/TASO/src/core/pad.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/pad.cc.o"
+  "/usr/TASO/src/core/pool2d.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/pool2d.cc.o"
+  "/usr/TASO/src/core/reduce.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/reduce.cc.o"
+  "/usr/TASO/src/core/reshape.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/reshape.cc.o"
+  "/usr/TASO/src/core/resize.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/resize.cc.o"
+  "/usr/TASO/src/core/shape.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/shape.cc.o"
+  "/usr/TASO/src/core/slice.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/slice.cc.o"
+  "/usr/TASO/src/core/split.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/split.cc.o"
+  "/usr/TASO/src/core/squeeze.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/squeeze.cc.o"
+  "/usr/TASO/src/core/substitution.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/substitution.cc.o"
+  "/usr/TASO/src/core/topk.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/topk.cc.o"
+  "/usr/TASO/src/core/transpose.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/transpose.cc.o"
+  "/usr/TASO/src/core/unsqueeze.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/unsqueeze.cc.o"
+  "/usr/TASO/src/core/where.cc" "/usr/TASO/build/CMakeFiles/taso_runtime.dir/src/core/where.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_CUDNN"
+  "taso_runtime_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../include"
+  "/opt/conda/include"
+  "/usr/local/cuda/include"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/usr/TASO/build/rules.pb.cc" "/usr/TASO/build/rules.pb.h"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
