1cc5f09fa9d4def10ed5fb6a9ecb58b5147c444e
hafizas101 <44812167+hafizas101@users.noreply.github.com>
2021-07-22 19:27:28 +0200

    Update nasrnn.py

--- examples/nasrnn.py
+++ examples/nasrnn.py
@@ -32,3 +32,6 @@ for i in range(length):
     state = nas_node(graph, state, xs[i])
 new_graph = taso.optimize(graph, alpha=1.0, budget=100)
 onnx_model = taso.export_onnx(new_graph)
+
+print(graph.run_time())
+print(new_graph.run_time())
