55dc919698812e0237f2e326104d1dc0e6701446
hafizas101 <44812167+hafizas101@users.noreply.github.com>
2021-07-22 18:02:32 +0200

    Add files via upload

---
+++ presentation_demo/optimize.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 17:50:48 2021
+
+@author: arslan
+"""
+import taso
+import onnx
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="Path to input ONNX file", required=True)
+
+args = parser.parse_args()
+
+
+graph = taso.load_onnx(args.file)
+new_graph = taso.optimize(graph, alpha = 1.0, budget = 100, print_subst=True)
+onnx_model = taso.export_onnx(new_graph)
+onnx.checker.check_model(onnx_model)
+onnx.save(onnx_model, "optimised_model.taso.onnx")
